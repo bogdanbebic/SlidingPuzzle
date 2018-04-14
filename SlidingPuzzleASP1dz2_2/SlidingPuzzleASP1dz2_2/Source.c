5bc76e5edf1087842c86e949de345b6559ec66ca
bogdanbebic <bb170011d@student.etf.bg.ac.rs>
2018-04-14 11:08:04 +0200

    Implemented puzzle solvability checking

--- SlidingPuzzleASP1dz2_2/SlidingPuzzleASP1dz2_2/Source.c
+++ SlidingPuzzleASP1dz2_2/SlidingPuzzleASP1dz2_2/Source.c
@@ -5,6 +5,11 @@
 #define MATRIX_SIZE 3
 #define MATRIX_ELEMENTS MATRIX_SIZE * MATRIX_SIZE
 
+#define MAX 381366965	// MAX changes if MATRIX_SIZE changes
+#define MIN 6053444	// MIN changes if MATRIX_SIZE changes
+
+#define BITS_IN_BYTE 8
+
 
 typedef struct StackElemStruct {
 	int value;
@@ -42,6 +47,7 @@ int isStackEmpty(StackElem **stack) {
 
 #pragma endregion
 
+
 #pragma region MATRIX_READING_AND_GENERATING
 
 void readMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
@@ -76,6 +82,84 @@ void generateMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
 
 #pragma endregion
 
+#pragma region PUZZLE_SOLVABILITY_CHECKING
+
+void findCellWithValue(int matrix[MATRIX_SIZE][MATRIX_SIZE], int *iEmpty, int *jEmpty, int cellValue) {
+	int i, j;
+	for (i = 0; i < MATRIX_SIZE; i++) {
+		for (j = 0; j < MATRIX_SIZE; j++) {
+			if (matrix[i][j] == cellValue) {
+				*iEmpty = i;
+				*jEmpty = j;
+				return;
+			}
+		}
+	}
+	return;
+}
+
+void matchEmptyCellsInBothMatrices(int start[MATRIX_SIZE][MATRIX_SIZE], int end[MATRIX_SIZE][MATRIX_SIZE]) {
+	int iEmptyStart, jEmptyStart;
+	int iEmptyEnd, jEmptyEnd;
+	int t;
+
+	findCellWithValue(start, &iEmptyStart, &jEmptyStart, 0);
+	findCellWithValue(end, &iEmptyEnd, &jEmptyEnd, 0);
+
+	while (iEmptyStart != iEmptyEnd || jEmptyStart != jEmptyEnd) {
+		if (iEmptyStart > iEmptyEnd) {
+			t = start[iEmptyStart][jEmptyStart];
+			start[iEmptyStart][jEmptyStart] = start[iEmptyStart - 1][jEmptyStart];
+			start[iEmptyStart - 1][jEmptyStart] = t;
+			iEmptyStart--;
+		}
+		if (iEmptyStart < iEmptyEnd) {
+			t = start[iEmptyStart][jEmptyStart];
+			start[iEmptyStart][jEmptyStart] = start[iEmptyStart + 1][jEmptyStart];
+			start[iEmptyStart + 1][jEmptyStart] = t;
+			iEmptyStart++;
+		}
+		if (jEmptyStart > jEmptyEnd) {
+			t = start[iEmptyStart][jEmptyStart];
+			start[iEmptyStart][jEmptyStart] = start[iEmptyStart][jEmptyStart - 1];
+			start[iEmptyStart][jEmptyStart - 1] = t;
+			jEmptyStart--;
+		}
+		if (jEmptyStart < jEmptyEnd) {
+			t = start[iEmptyStart][jEmptyStart];
+			start[iEmptyStart][jEmptyStart] = start[iEmptyStart][jEmptyStart + 1];
+			start[iEmptyStart][jEmptyStart + 1] = t;
+			jEmptyStart++;
+		}
+	}
+	return;
+}
+
+int isSolutionPossible(int start[MATRIX_SIZE][MATRIX_SIZE], int end[MATRIX_SIZE][MATRIX_SIZE]) {
+	int i, j;
+	int i1, j1;
+	int counter = 0;
+	int t;
+
+	matchEmptyCellsInBothMatrices(start, end);
+	
+	for (i = 0; i < MATRIX_SIZE; i++) {
+		for (j = 0; j < MATRIX_SIZE; j++) {
+			if (start[i][j] != end[i][j]) {
+				findCellWithValue(start, &i1, &j1, end[i][j]);
+				t = start[i][j];
+				start[i][j] = start[i1][j1];
+				start[i1][j1] = t;
+				counter++;
+			}
+		}
+	}
+	return !(counter % 2);
+}
+
+#pragma endregion
+
+
 void printMenu() {
 	printf("1. Enter start and end positions for the puzzle\n");
 	printf("2. Generate start and end positions for the puzzle\n");
@@ -86,10 +170,14 @@ void printMenu() {
 	return;
 }
 
+
+char set[MAX - MIN];
+
 int main() {
 	int menuOption;
 	int start[MATRIX_SIZE][MATRIX_SIZE];
 	int end[MATRIX_SIZE][MATRIX_SIZE];
+	int flag = 0;
 	
 	srand((int)time(NULL));
 
@@ -102,16 +190,30 @@ int main() {
 			readMatrix(start);
 			printf("Enter end matrix:\n");
 			readMatrix(end);
+			flag = 1;
 			break;
 		case 2:	// Generate start and end matrix
 			generateMatrix(start);
 			generateMatrix(end);
+			flag = 1;
 			break;
 		case 3:	// Check if solution exists
-			/* TODO: implement */
+			if (flag) {
+				if (isSolutionPossible(start, end)) {
+					printf("Solution is possible!\n");
+				}
+				else {
+					printf("The puzzle is unsolvable :(\n");
+				}
+			}
 			break;
 		case 4:	// Simulate puzzle
-			/* TODO: implement */
+			if (flag && isSolutionPossible(start, end)) {
+				/* TODO: implement */
+			}
+			else {
+				printf("There is no puzle or it is unsolvable\n");
+			}
 			break;
 		case 0:	// Exit program
 			/* TODO: deallocation */
